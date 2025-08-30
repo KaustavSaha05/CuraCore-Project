3c085c7c73c28f806eca065a959358f262dce7a6
Kaustav Saha <kaustav.saha04@gmail.com>
2025-08-30 14:16:21 +0530

    files updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
+  server: {
+    https: true
+  }
 })
